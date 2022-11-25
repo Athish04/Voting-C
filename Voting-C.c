9782af32a2522138fe03a547bdf63d3e76a4f9ee
Athish I.S <117496005+Athish04@users.noreply.github.com>
2022-11-25 12:04:35 +0530

    Add files via upload

---
+++ Voting-C.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("\nEnter your age");
+scanf("%d",&a);
+if(a<18)
+{
+printf("/nYou are not eligible to vote");
+}
+else
+{
+printf("\nYou are eligible to vote");
+}
+
+}
+
